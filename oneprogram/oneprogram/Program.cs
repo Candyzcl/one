ec6b9cf57a2ab3e555dd0aa74003922b780e2226
Candyzcl <2596924715@qq.com>
2020-06-24 08:24:06 +0800

    first

---
+++ oneprogram/oneprogram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oneprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
