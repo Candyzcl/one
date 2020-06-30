9cb0d3c6224d708fc822756fdecbb1f725beefa5
Candyzcl <2596924715@qq.com>
2020-06-30 21:48:43 +0800

    my

---
+++ My/My/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace My
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBoxOp2 = new System.Windows.Forms.TextBox();
+            this.textBoxOp1 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelResult1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(58, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(926, 526);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBoxOp2);
+            this.panel4.Controls.Add(this.textBoxOp1);
+            this.panel4.Location = new System.Drawing.Point(85, 156);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(448, 156);
+            this.panel4.TabIndex = 2;
+            // 
+            // textBoxOp2
+            // 
+            this.textBoxOp2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxOp2.Location = new System.Drawing.Point(19, 87);
+            this.textBoxOp2.Name = "textBoxOp2";
+            this.textBoxOp2.Size = new System.Drawing.Size(386, 53);
+            this.textBoxOp2.TabIndex = 1;
+            // 
+            // textBoxOp1
+            // 
+            this.textBoxOp1.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBoxOp1.Location = new System.Drawing.Point(19, 23);
+            this.textBoxOp1.Name = "textBoxOp1";
+            this.textBoxOp1.Size = new System.Drawing.Size(386, 53);
+            this.textBoxOp1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(596, 79);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(117, 369);
+            this.panel3.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(19, 275);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 46);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "=";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(19, 196);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(19, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(19, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.labelResult1);
+            this.panel2.Location = new System.Drawing.Point(85, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(464, 65);
+            this.panel2.TabIndex = 0;
+            // 
+            // labelResult1
+            // 
+            this.labelResult1.AutoSize = true;
+            this.labelResult1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelResult1.Location = new System.Drawing.Point(18, 13);
+            this.labelResult1.Name = "labelResult1";
+            this.labelResult1.Size = new System.Drawing.Size(85, 31);
+            this.labelResult1.TabIndex = 0;
+            this.labelResult1.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 592);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBoxOp2;
+        private System.Windows.Forms.TextBox textBoxOp1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelResult1;
+    }
+}
+
---
+++ My/My/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x + y;
+            this.labelResult1.Text = z.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x - y;
+            this.labelResult1.Text = z.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            var z = x * y;
+            this.labelResult1.Text = z.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(this.textBoxOp1.Text);
+            var y = int.Parse(this.textBoxOp2.Text);
+            if (y != 0)
+            {
+                var z = x / y;
+                this.labelResult1.Text = z.ToString();
+            }
+            else {
+                this.labelResult1.Text = "0不能作为除数";
+            }
+            
+        }
+    }
+}
